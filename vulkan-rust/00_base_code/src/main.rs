c467e70e32a80b67cf0281ff1638296a1d9a51df
Jack <wangfangsiqi@wondersgroup.com>
2024-05-13 16:01:26 +0800

    feat 优化代码

--- vulkan-rust/00_base_code/src/main.rs
+++ vulkan-rust/00_base_code/src/main.rs
@@ -1,3 +1,52 @@
+extern crate vulkano;
+extern crate winit;
+
+use winit::{dpi::LogicalSize, Event, EventsLoop, WindowBuilder, WindowEvent};
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
+
+#[allow(unused)]
+struct HelloTriangleApplication {
+    events_loop: EventsLoop,
+}
+
+impl HelloTriangleApplication {
+    pub fn initialize() -> Self {
+        let events_loop = Self::init_window();
+
+        Self { events_loop }
+    }
+
+    fn init_window() -> EventsLoop {
+        let events_loop = EventsLoop::new();
+        let _window = WindowBuilder::new()
+            .with_title("Vulkan")
+            .with_dimensions(LogicalSize::new(f64::from(WIDTH), f64::from(HEIGHT)))
+            .build(&events_loop);
+        events_loop
+    }
+
+    fn main_loop(&mut self) {
+        loop {
+            let mut done = false;
+            self.events_loop.poll_events(|ev| {
+                if let Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } = ev
+                {
+                    done = true
+                }
+            });
+            if done {
+                return;
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = HelloTriangleApplication::initialize();
+    app.main_loop();
 }
