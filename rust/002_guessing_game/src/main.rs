2f006f28a5078750120f111c503eb3662140e466
Jack <wangfangsiqi@wondersgroup.com>
2024-05-27 14:19:45 +0800

    feat 输出 第二种 方法

--- rust/002_guessing_game/src/main.rs
+++ rust/002_guessing_game/src/main.rs
@@ -10,4 +10,9 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("读取行失败。");
 
     println!("你猜到了：{guess}");
+
+    let x = 5;
+    let y = 10;
+
+    println!("x = {} and y + 2 = {}", x, y + 2);
 }
