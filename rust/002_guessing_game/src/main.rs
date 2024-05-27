15646967bcb30e020a6a1d1721732545f5d35d44
Jack <wangfangsiqi@wondersgroup.com>
2024-05-27 16:24:13 +0800

    feat 猜数字游戏

--- rust/002_guessing_game/src/main.rs
+++ rust/002_guessing_game/src/main.rs
@@ -1,20 +1,35 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("猜猜正整数！");
+    println!("猜猜正整数 1 ~ 100 ！");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("秘密号码是：{}", secret_number);
+    // println!("秘密号码是：{}。", secret_number);
 
-    println!("请输入你的猜测。");
+    loop {
+        println!("请输入你的猜测。");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("读取行失败。");
+        io::stdin().read_line(&mut guess).expect("读取失败。");
 
-    let guess: u32 = guess.trim().parse().expect("请输入一个正整数！");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("你猜到了：{guess}");
+        // println!("你的猜测：{guess}。");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小 ！"),
+            Ordering::Greater => println!("太大 ！"),
+            Ordering::Equal => {
+                println!("你赢了 ！");
+                break;
+            }
+        }
+    }
 }
